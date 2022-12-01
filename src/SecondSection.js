38934fa573138c7e6c359f8efa14cbcc481933c1
SamanthaShackelford <samantha@echobind.com>
2022-12-01 14:28:23 -0500

    Add second and third sections

---
+++ src/SecondSection.js
@@ -0,0 +1,10 @@
+import { Html } from "@react-three/drei"
+
+export const SecondSection = ({ position }) => {
+  return (
+    <Html position={position}>
+      SecondSection
+    </Html>
+  )
+}
+
---
+++ src/ThirdSection.js
@@ -0,0 +1,11 @@
+import { Html } from "@react-three/drei"
+
+
+export const ThirdSection = ({ position }) => {
+  return (
+    <Html position={position}>
+      ThirdSection
+    </Html>
+  )
+}
+
