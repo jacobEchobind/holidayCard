4703e03b7cab21a21c2fded20c7634307c028ca2
Jacob Galito <jacob@echobind.com>
2022-12-06 10:47:16 -0500

    Mesh compression, Postprocessing, Snow animation

--- src/Experience.js
+++ src/Experience.js
@@ -1,36 +1,105 @@
-import { useThree } from '@react-three/fiber'
-import { 
-    PresentationControls, 
-    Float, 
-    Environment, 
-} from '@react-three/drei'
-import { EffectComposer, DepthOfField } from '@react-three/postprocessing';
-import { Portal } from './Portal'
+import { useFrame, useThree } from '@react-three/fiber'
+import { useState, useRef, Suspense } from 'react'
+import { Instances, Instance, useGLTF, PresentationControls, Float, Environment } from '@react-three/drei'
+import { TiltShift, Bloom, Noise, Vignette, EffectComposer, DepthOfField } from '@react-three/postprocessing'
+import { BlendFunction } from 'postprocessing'
+import * as THREE from 'three'
+import { Perf } from 'r3f-perf'
+import { Portal } from './Portal.js'
 
-export default function Experience({ position }) {
+
+function Snow({z}) {
+    const ref = useRef()
+    // const [clicked, setClicked] = useState(false)
+    const { viewport, camera } = useThree()
+    const { width, height } = viewport.getCurrentViewport(camera, [0, 0, z])
+    const {nodes, materials} =  useGLTF('/meshes/snowflake.glb')
+  
+    const [data] = useState({
+      x: THREE.MathUtils.randFloatSpread(2), // example value: 6 which is -3 to 3
+      y: THREE.MathUtils.randFloatSpread(height),
+      rX: Math.random() * Math.PI,
+      rY: Math.random() * Math.PI,
+      rZ: Math.random() * Math.PI,
+    })
+  
+    useFrame((state) => {
+      ref.current.rotation.set((data.rX += 0.001), (data.rY += 0.004), (data.rZ += 0.0005))
+      ref.current.position.set(data.x * width, (data.y -= Math.sin(0.08) * 1.33), z)
+      if (data.y < - height / 1.5) {
+         data.y = height / .25
+      }
+    })
+  
+    return (<mesh ref={ref} castShadow receiveShadow geometry={nodes.Snowflake_low_1.geometry} material={materials["Ice Imperfections"]} />)
+    // material-emissive={"orange"} //
+  }
+
+export default function Experience({ position, FocalLength, FocusDistance, BokehScale }) {
     const { viewport } = useThree();
+    const count = 150;
+    const depth = 40;
 
     return (
         <group position={ position }>
             <Environment preset='city'/>
-            <ambientLight intensity={.1} />
-            <pointLight position={[10, 10, 10]} intensity={2} />
-            
-            {/* these controls are a helper from react three drei and is used instead of orbit controls */}
-            <PresentationControls 
-                // if you add global the presentation controls will be added to the whole canvas vs just meshes in the canvas
-                rotation={ [ 0.13, -0.1, 0 ] }
-                polar={ [ -0.4, 0.2 ] }
-                azimuth={ [ -1, 0.75 ] }
-                config={ { mass: 2, tension: 400 } }
-                // counter weight and on release will return to initial position
-                // snap={ { mass: 4, tension: 400 }  }
-            >
-                <Float rotationIntensity={ 0.4 }>
-                    {/* Picture frame with trees scene model */}
-                    <Portal scale={viewport.width / 10}/>
-                </Float>
-            </PresentationControls>
+            <ambientLight intensity={.8} />
+            <spotLight position={[10, 10, 10]} intensity={3} />
+            <Suspense fallback={ null }>
+                
+                <EffectComposer>
+                    {/* <TiltShift /> */}
+                    <Bloom
+                        luminanceThreshold={.65} // 0 - 1 range https://github.com/pmndrs/react-postprocessing#selective-bloom
+                        luminanceSmoothing={.75}
+                        height={180}
+                        opacity={1.5}
+                    />
+                    {/* <Noise opacity={0.001} />   */}
+                    <Vignette
+                        offset={0.2}
+                        darkness={0.9}
+                        // Eskil's vignette technique works from the outside inwards rather
+                        // than the inside outwards, so if this is 'true' set the offset
+                        // to a value greater than 1.
+                        // See frag for details - https://github.com/vanruesc/postprocessing/blob/main/src/effects/glsl/vignette/shader.frag
+                        eskil={false}
+                        // blendFunction={BlendFunction.SOFT_LIGHT}
+                    />
+                    <DepthOfField 
+                        target={[0, 0, 30]} 
+                        focalLength={ 1.5 } // focal length
+                        bokehScale={ 3 } // bokeh size
+                        height={1000} 
+                        focusDistance={ 8 } // where to focus
+                    />
+                </EffectComposer>
+                {Array.from({length: count}, (_, i) => (
+                        <Snow key={i} z={-(i / count) * depth -35} 
+                    />
+                    ))}
+                {/* these controls are a helper from react three drei and is used instead of orbit controls */}
+                <PresentationControls 
+                    // if you add global the presentation controls will be added to the whole canvas vs just meshes in the canvas
+                    rotation={ [ 0.13, -0.1, 0 ] }
+                    polar={ [ -0.4, 0.2 ] }
+                    azimuth={ [ -1, 0.75 ] }
+                    config={ { mass: 2, tension: 400 } }
+                    // counter weight and on release will return to initial position
+                    // snap={ { mass: 4, tension: 400 }  }
+                >
+                    <Float rotationIntensity={ 0.8 }>
+                        {/* Picture frame with trees scene model */}
+                        <Portal scale={viewport.width / 10}/>
+                    </Float>
+                </PresentationControls>
+                <Environment preset='city'/>
+            </Suspense>
+            <Perf position='top-left' />
         </group> 
     )
 }
+
+// export const FocalLength = 'FocalLength';
+// export const FocusDistance = 'FocusDistance';
+// export const BokehScale = 'BokehScale';
--- src/Hero.js
+++ src/Hero.js
@@ -6,37 +6,38 @@ import { useRef, useState } from "react"
 import { useFrame } from '@react-three/fiber'
 import { folder, useControls } from 'leva'
 import { data } from './store'
+import { FocalLength, FocusDistance, BokehScale } from './Experience.js'
 
-function Flakes({ data, range, scale }) {
-    const { nodes, materials } = useGLTF('./meshes/snowflake.glb')
-    return (
-      <Instances range={ range } scale={ scale } material={materials['Ice Imperfections']}  geometry={nodes.Snowflake_low_1.geometry}>
-        <group position={[0, 0, 0]}>
-          {data.map((props, i) => (
-            <Flake key={i} {...props} />
-          ))}
-        </group>
-      </Instances>
-    )
-}
+// function Flakes({ data, range, scale }) {
+//     const { nodes, materials } = useGLTF('./meshes/snowflake.glb')
+//     return (
+//       <Instances range={ range } scale={ scale } material={materials['Ice Imperfections']}  geometry={nodes.Snowflake_low_1.geometry}>
+//         <group position={[0, 0, 0]}>
+//           {data.map((props, i) => (
+//             <Flake key={i} {...props} />
+//           ))}
+//         </group>
+//       </Instances>
+//     )
+// }
 
 
-function Flake({ random, color = new THREE.Color(), ...props }) {
-    const ref = useRef()
-    const [ hovered, setHover ] = useState(false)
-    useFrame((state) => {
-        const t = state.clock.getElapsedTime() + random * 10000
-        ref.current.rotation.set(Math.cos(t / 4) / 2, Math.sin(t / 4) / 2, Math.cos(t / 1.5) / 2)
-        ref.current.position.y = Math.sin(t / 1.5) / 2
-        ref.current.scale.x = ref.current.scale.y = ref.current.scale.z = THREE.MathUtils.lerp(ref.current.scale.z, hovered ? 0.08 : 0.033, 0.05)
-        ref.current.color.lerp(color.set(hovered ? '#D4F4F5' : '#E7F5F6'), hovered ? .01 : 0.5)
-  })
-  return (
-    <group {...props}>
-      <Instance scale={ 0.01 } ref={ref} onPointerOver={(e) => (e.stopPropagation(), setHover(true))} onPointerOut={(e) => setHover(false)} />
-    </group>
-  )
-}
+// function Flake({ random, color = new THREE.Color(), ...props }) {
+//     const ref = useRef()
+//     const [ hovered, setHover ] = useState(false)
+//     useFrame((state) => {
+//         const t = state.clock.getElapsedTime() + random * 10000
+//         ref.current.rotation.set(Math.cos(t / 4) / 2, Math.sin(t / 4) / 2, Math.cos(t / 1.5) / 2)
+//         ref.current.position.y = Math.sin(t / 1.5) / 2
+//         ref.current.scale.x = ref.current.scale.y = ref.current.scale.z = THREE.MathUtils.lerp(ref.current.scale.z, hovered ? 0.08 : 0.033, 0.05)
+//         ref.current.color.lerp(color.set(hovered ? '#D4F4F5' : '#E7F5F6'), hovered ? .01 : 0.5)
+//   })
+//   return (
+//     <group {...props}>
+//       <Instance scale={ 0.01 } ref={ref} onPointerOver={(e) => (e.stopPropagation(), setHover(true))} onPointerOut={(e) => setHover(false)} />
+//     </group>
+//   )
+// }
 
 export default function Hero() {
 
@@ -85,10 +86,10 @@ export default function Hero() {
                 SubPositionZ: { value: 0, min: -3, max: 3 },
             }),
 
-            Snowflakes: folder ({ 
-                SnowRange: { value: 100, min: 0, max: 300}, 
-                SnowScale: { value: 1, min: 0, max: 300} 
-            }),
+            // Snowflakes: folder ({ 
+            //     SnowRange: { value: 100, min: 0, max: 300}, 
+            //     SnowScale: { value: 1, min: 0, max: 300} 
+            // }),
 
             FloatAnimation: folder ({
                 FloatSpeed: { value: 1, min: 0, max: 20 },
@@ -109,15 +110,20 @@ export default function Hero() {
                 CloudPositionY: { value: 6, min: -50, max: 50  }, // Y position
                 CloudPositionZ: { value: -4, min: -30, max: 0  }, // Z position
                 CloudColor: { value: '#F1F9FA' } // Cloud Color
-             })
+             }),
+
+             DOF: folder ( {
+                FocusDistance: { min: 0, max: 4, value: 2 },
+                FocalLength: { min: 0, max: 1, value: 0.1 },
+                BokehScale: { min: 0, max: 10, value: 2 }
+            })
         }
     )
 
     // const header = useGLTF('./meshes/happy_holidays.glb')
     const header1 = useGLTF('./meshes/happy.glb')
     const header2 = useGLTF('./meshes/holidays.glb')
-
-    const EBlogo = useGLTF('./meshes/EBLogo_snow.glb')
+    const EBLogo = useGLTF('./meshes/EBLogo.glb')
 
     return <>
         
@@ -161,17 +167,17 @@ export default function Hero() {
                 From all of us at
             </Text>
             <primitive 
-                object={ EBlogo.scene } 
+                object={ EBLogo.scene } 
                 position={ [ EBPositionX, EBPositionY, EBPositionZ ] }
                 scale={ EBScale }
                 rotation={ [ EBRotationX, EBRotationY, 0 ] }
             />
         </Float>
-        <Flakes 
+        {/* <Flakes 
                 data={ data } 
                 range={ SnowRange } 
                 scale={ SnowScale } 
-        />
+        /> */}
         {/* <Cloud
             opacity={ CloudOpacity } // Cloud opacity
             speed={ CloudSpeed } // Rotation speed
