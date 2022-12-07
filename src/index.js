4acf950bfff7b10228b48505f77d83f25ab0d916
SamanthaShackelford <samantha@echobind.com>
2022-12-07 11:06:07 -0500

    Add fonts and Loader

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
+import { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
-import { Loader } from '@react-three/drei'
+import { Loader } from './Loader'
+
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
 root.render(
     <>
         <Canvas camera={{ near: 0.1, far: 200, fov: 80}}>
-            <App />
+            <Suspense fallback={<Loader/>}>
+                <App />
+            </Suspense>
         </Canvas>
-        <Loader/>
     </>
 )
