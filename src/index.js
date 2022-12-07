01605f682a3b1fc9410a9d0e6a149ef05da5f717
SamanthaShackelford <samantha@echobind.com>
2022-12-07 10:03:23 -0500

    add loader

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 
 import { Canvas } from '@react-three/fiber'
+import { Loader } from '@react-three/drei'
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
 root.render(
-    <Canvas camera={{ near: 0.1, far: 200, fov: 80}}>
-        <App />
-    </Canvas>
+    <>
+        <Canvas camera={{ near: 0.1, far: 200, fov: 80}}>
+            <App />
+        </Canvas>
+        <Loader/>
+    </>
 )
