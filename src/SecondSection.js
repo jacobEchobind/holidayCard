1bb4260c49372cbc4803ce156980917bd22638c7
SamanthaShackelford <samantha@echobind.com>
2022-12-08 10:36:05 -0500

    add background and use proxima nova

--- src/SecondSection.js
+++ src/SecondSection.js
@@ -9,60 +9,69 @@ export const SecondSection = ({ position }) => {
   const breakpoint = 4.5;
 
   return (
-    <Flex  
-      dir='column'
-      position={position} 
-      size={[viewport.width, viewport.height, 0]} // xyz size to constrain content to
-      alignItems='center'
-      centerAnchor={true}
-    >
-      <Box
-        dir='row'
-        justifyContent='center'
-        alignItems='center'
-        centerAnchor={false}
-        flexWrap='wrap'
-        width='100%'
-        height='100%'
-      >
-        <Box 
-          width={1}
+    <>
+      <mesh scale={[viewport.width, viewport.height / 3, 1]} position={position} >
+        <planeGeometry />
+        <meshPhongMaterial color={'lightblue'} depthTest={false} />
+      </mesh>
+      <group position={[0, .5, 0]}>
+        <Flex  
+          dir='column'
+          position={position} 
+          size={[viewport.width, viewport.height, 0]} // xyz size to constrain content to
+          alignItems='center'
           centerAnchor={true}
-          marginTop={viewport.width > breakpoint ? .4 : .5}
         >
-          <Sphere args={[.3, 16, 16]}>
-              <meshLambertMaterial attach="material" color="white" />
-          </Sphere>
-        </Box>
-        
-        <Box 
-          centerAnchor={true}
-          marginLeft={viewport.width > breakpoint ? .4 : 0}
-        >
-            <Text 
-              color='white' 
-              scale={4}
-              maxWidth={(viewport.width > breakpoint) ? 3 : 1}
-              font='./fonts/christmas-squad.otf'
+          <Box
+            dir='row'
+            justifyContent='center'
+            alignItems='center'
+            centerAnchor={false}
+            flexWrap='wrap'
+            width='100%'
+            height='100%'
+          >
+            <Box 
+              width={1}
+              centerAnchor={true}
+              marginTop={viewport.width > breakpoint ? .4 : .5}
             >
-              Second Section
-            </Text>
-            <Text 
-              anchorY="top"
-              color='white'
-              position={[0, -0.3, 0]}
-              textAlign='left'
-              maxWidth={viewport.width > breakpoint ? 2 : 1.75}
-              scale={2}
-              font='./fonts/campton-light.otf'
-
+              <Sphere args={[.3, 16, 16]}>
+                  <meshLambertMaterial attach="material" color="darkgreen" />
+              </Sphere>
+            </Box>
+            
+            <Box 
+              centerAnchor={true}
+              marginLeft={viewport.width > breakpoint ? .4 : 0}
             >
-              Nullam viverra, mauris quis imperdiet gravida, nunc risus mollis enim, eu molestie risus turpis in ante. Nullam molestie sapien quis fermentum rhoncus.
-            </Text>
-        </Box>
+                <Text 
+                  color='black' 
+                  scale={4}
+                  maxWidth={(viewport.width > breakpoint) ? 3 : 1}
+                  font='./fonts/christmas-squad.otf'
+                >
+                  Second Section
+                </Text>
+                <Text 
+                  anchorY="top"
+                  color='black'
+                  position={[0, -0.3, 0]}
+                  textAlign='left'
+                  maxWidth={viewport.width > breakpoint ? 2 : 1.75}
+                  scale={2}
+                  font='./fonts/proxima-nova.otf'
 
-      </Box>
-    </Flex>
+                >
+                  Nullam viverra, mauris quis imperdiet gravida, nunc risus mollis enim, eu molestie risus turpis in ante. Nullam molestie sapien quis fermentum rhoncus.
+                </Text>
+            </Box>
+
+          </Box>
+        </Flex>
+      </group>
+    </>
+   
   )
 }
 
--- src/ThirdSection.js
+++ src/ThirdSection.js
@@ -7,54 +7,62 @@ export const ThirdSection = ({ position }) => {
   const breakpoint = 4.5
 
   return (
-    <Flex  
-      dir={'column'}
-      position={position} 
-      size={[viewport.width, viewport.height, 0]} // xyz size to constrain content to
-      alignItems='center'
-      centerAnchor={true}
-    >
-      <Box
-        dir='row'
-        justifyContent='center'
-        alignItems='center'
-        centerAnchor={false}
-        flexWrap='wrap'
-        width='100%'
-        height='100%'
-      >
-        <Box centerAnchor={true}>
-          <Text 
-            color='white' 
-            scale={4}
-            maxWidth={(viewport.width > breakpoint) ? 3 : 1}
-            font='./fonts/christmas-squad.otf'
-          >
-            Third Section
-          </Text>
-          <Text 
-            maxWidth={viewport.width > breakpoint ? 2 : 1.75}
-            anchorY="top"
-            position={[0, -0.3, 0]}
-            color='white'
-            scale={2}
-            font='./fonts/campton-light.otf'
-          >
-            Nullam viverra, mauris quis imperdiet gravida, nunc risus mollis enim, eu molestie risus turpis in ante. Nullam molestie sapien quis fermentum rhoncus.
-          </Text>
-        </Box>
-        <Box
-          width={1}
+    <>
+      <mesh scale={[viewport.width, viewport.height / 3, 1]} position={position} >
+        <planeGeometry />
+        <meshPhongMaterial color={'lightblue'} depthTest={false} />
+      </mesh>
+      <group position={[0, .5, 0]}>
+        <Flex  
+          dir={'column'}
+          position={position} 
+          size={[viewport.width, viewport.height, 0]} // xyz size to constrain content to
+          alignItems='center'
           centerAnchor={true}
-          marginTop={viewport.width > breakpoint ? 1.5 : 1.5}
-          marginLeft={viewport.width > breakpoint ? .4 : 0}
         >
-          <Sphere args={[.3, 16, 16]}>
-            <meshLambertMaterial attach="material" color="coral" />
-          </Sphere>
-        </Box>
-      </Box>
-    </Flex>
+          <Box
+            dir='row'
+            justifyContent='center'
+            alignItems='center'
+            centerAnchor={false}
+            flexWrap='wrap'
+            width='100%'
+            height='100%'
+          >
+            <Box centerAnchor={true}>
+              <Text 
+                color='black' 
+                scale={4}
+                maxWidth={(viewport.width > breakpoint) ? 3 : 1}
+                font='./fonts/christmas-squad.otf'
+              >
+                Third Section
+              </Text>
+              <Text 
+                maxWidth={viewport.width > breakpoint ? 2 : 1.75}
+                anchorY="top"
+                position={[0, -0.3, 0]}
+                color='black'
+                scale={2}
+                font='./fonts/proxima-nova.otf'
+              >
+                Nullam viverra, mauris quis imperdiet gravida, nunc risus mollis enim, eu molestie risus turpis in ante. Nullam molestie sapien quis fermentum rhoncus.
+              </Text>
+            </Box>
+            <Box
+              width={1}
+              centerAnchor={true}
+              marginTop={viewport.width > breakpoint ? 1.5 : 1.5}
+              marginLeft={viewport.width > breakpoint ? .4 : 0}
+            >
+              <Sphere args={[.3, 16, 16]}>
+                <meshLambertMaterial attach="material" color="red" />
+              </Sphere>
+            </Box>
+          </Box>
+        </Flex>
+      </group>
+    </>
   )
 }
 
