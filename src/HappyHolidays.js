2438ac8206c0cbf15c526fa57cdd6c0797e971f4
SamanthaShackelford <samantha@echobind.com>
2022-12-09 10:04:20 -0500

    Add bevel to font

--- src/HappyHolidays.js
+++ src/HappyHolidays.js
@@ -2,12 +2,12 @@ import  { useRef } from 'react'
 import { extend, useThree } from '@react-three/fiber' // extends three-fiber's catalogue of JSX elements
 import { FontLoader } from 'three/examples/jsm/loaders/FontLoader' // loads fonts from a JSON file
 import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry'
-import inter from './assets/Inter.json'
+import Inter from './assets/Inter.json'
 
 extend({ TextGeometry })
 
 export default function HappyHolidays() {
-  const font = new FontLoader().parse(inter);
+  const font = new FontLoader().parse(Inter);
   const { viewport } = useThree();
   const mesh = useRef();
   let textWidth
@@ -26,9 +26,14 @@ export default function HappyHolidays() {
       <textGeometry args={['Happy Holidays', {
         font, 
         size: viewport.width > 3 ? .4 : viewport.width / 12 , 
-        height: .2
+        height: .15,
+        bevelEnabled: true,
+        bevelThickness: .1,
+        bevelSize: .01,
+        bevelOffset: .001,
+        bevelSegments: 3
       }]} />
-      <meshPhysicalMaterial attach='material' color='red' />
+      <meshPhysicalMaterial attach='material' color='teal' />
     </mesh>
   )
 }
